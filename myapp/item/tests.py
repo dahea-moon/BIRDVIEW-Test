e4593cd235435aa999b0d193ac5a8cb35f4afcc3
Dahea Moon <dahea0512@gmail.com>
2020-01-26 23:50:26 +0900

    test codes done

--- myapp/item/tests.py
+++ myapp/item/tests.py
@@ -1,85 +1,92 @@
-# from django.urls import reverse
-# from django.test import TestCase
-# from rest_framework.test import APIRequestFactory, APITestCase, APIClient
+from django.urls import reverse
 
-# import random
+from rest_framework.test import APITestCase
 
-# from myapp.item.models import Product, Ingredient
+from myapp.item.models import Product, Ingredient
 
 
-# class ProductListTest(APITestCase):
+class ProductAPITest(APITestCase):
+    
+    fixtures = ['ingredients-data.json', 'products-data.json']
 
-#     def test_list_with_pagination(self):
-#         url = reverse('item:product_list')
-#         data = {
-#             'skin_type': 'oily',
-#             'page': 5,
-#         }
-#         response = self.client.get(url, data, content_type='application/json')
-#         self.assertEquals(response.status_code, 200)
-#         self.assertEqauls(response.count, 1000)
-#         self.assertEquals(len(response.results), 50)
-#         self.assertEquals(response.results[0].id, 923)
+    def test_list_with_pagination(self):
+        url = reverse('item:product_list')
+        data = {
+            'skin_type': 'oily',
+            'page': 5,
+        }
+        response = self.client.get(url, data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['count'], 1000)
+        self.assertEqual(len(response.data['results']), 50)
+        self.assertEqual(response.data['results'][0]['id'], 923)
 
     
-#     def test_list_with_pagination_category(self):
-#         url = reverse('item:product_list')
-#         data = {
-#             'skin_type': 'sensitive',
-#             'page': 2,
-#             'category': 'basemakeup'
-#         }
-#         response = self.client.get(url, data=data, format='json')
-#         self.assertEquals(response.status_code, 200)
-#         self.assertEqauls(response.count, 242)
-#         self.assertEquals(len(response.results), 50)
-#         self.assertEquals(response.results[48].id, 444)
+    def test_list_with_pagination_category(self):
+        url = reverse('item:product_list')
+        data = {
+            'skin_type': 'sensitive',
+            'page': 2,
+            'category': 'basemakeup'
+        }
+        response = self.client.get(url, data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['count'], 242)
+        self.assertEqual(len(response.data['results']), 50)
+        self.assertEqual(response.data['results'][48]['id'], 444)
 
 
-    # def test_list_with_iclude_ingredients(self):
+    def test_list_with_iclude_ingredients(self):
+
+        url = reverse('item:product_list')
+        data = {
+            'skin_type': 'dry',
+            'page': 1,
+            'include_ingredient': 'screw,multimedia'
+            }
+        response = self.client.get(url, data, content_type='application/json')
+
+        count = 0
+        if response.data['results']:
+            for product in response.data['results']:
+                ingredients = product['ingredients'].split(',')
+                if 'screw' in ingredients and 'multimedia' in ingredients:
+                    count += 1
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(count, len(response.data['results']))
+    
+
+    def test_list_with_exclude_ingredients(self):
+
+        url = reverse('item:product_list')
+        data = {
+            'skin_type': 'oily',
+            'page': 1,
+            'exclude_ingredient': 'unique,waiter'
+        }
+        response = self.client.get(url, data, content_type='application/json')
         
-    #     for i in range(1, 3):
-    #         id = random.randint(1, 1001)
-    #         f'ingredient{i}' = Ingredient.objects.get(id=id).name
-    #     print(ingredient1)
-    #     print(ingredient2)
-
-    #     url = reverse('product_list')
-    #     data = {
-    #         'skin_type': 'dry',
-    #         'page': 1,
-    #         'include_ingredient': ingredient1,ingredient2
-    #     }
-    #     response = self.client.get(url, data, format='json')
-
-    #     flag = False
-    #     for product in response.result:
-    #         ingredients = product.ingredients
-    #         if ingredient1 in ingredients and ingredient2 in ingredients:
-    #             flag = True
-
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(flag, True)
+        count = 0
+        if response.data['results']:
+            for product in response.data['results']:
+                ingredients = product['ingredients'].split(',')
+                if 'unique' in ingredients or 'waiter' in ingredients:
+                    count += 1
+ 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(count, 0)
     
-    # def test_list_with_exclude_ingredients(self):
 
-    #     for i in range(1, 3):
-    #         id = random.randint(1, 1001)
-    #         f'ingredient{i}' = Ingredient.objects.get(id=id).name
+    def test_product_detail(self):
+        url = reverse('item:product_detail', kwargs={'product_id': 10})
+        data = {
+            'skin_type': 'oily',
+        }
+        response = self.client.get(url, data, content_type='application/json')
 
-    #     url = reverse('product_list')
-    #     data = {
-    #         'skin_type': 'oily',
-    #         'page': 1,
-    #         'exclude_ingredient': ingredient1,ingredient2
-    #     }
-    #     response = self.client.get(url, data, format='json')
-
-    #     flag = True
-    #     for product in response.result:
-    #         ingredients = product.ingredients
-    #         if ingredient1 in ingredients or ingredient2 in ingredients:
-    #             flag = False
-
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(flag, True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['id'], 10)
+        self.assertEqual(len(response.data['recommends']), 3)
